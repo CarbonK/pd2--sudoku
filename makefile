1d29f31c1372725ba869c9668ce810b2397c0016
Zong-You Xie <i1007673219@hotmail.com>
2016-04-03 21:25:00 +0800

    Upload again

--- makefile
+++ makefile
@@ -1,2 +1,6 @@
-all:
-	g++ Sudoku.cpp solve.cpp transform.cpp giveQuestion.cpp test.cpp
+all: Sudoku.o giveQuestion.cpp solve.cpp transform.cpp
+	g++ giveQuestion.cpp Sudoku.o -o giveQuestion
+	g++ solve.cpp Sudoku.o -o solve
+	g++ transform.cpp Sudoku.o -o transform
+Sudoku.o: Sudoku.cpp Sudoku.h
+	g++ -c Sudoku.cpp -o Sudoku.o
--- solve.cpp
+++ solve.cpp
@@ -1,34 +1,8 @@
 #include "Sudoku.h"
-#include <cstdio>
-#include <algorithm>
 using namespace std;
-
-void Sudoku::solve(){
-	int prespace = 81;
-
-	if(!solutionCheck()){puts("0");}
-	else if(solutionCheck() > 64){puts("2");}
-	else{
-		initMap();
-		while(1){
-			modifyQ();
-			if(spaceCheck() == prespace){break;}
-			else{prespace = spaceCheck();}
-		}
-		switch(recur(0)){
-			case 0:
-				puts("0");
-				break;
-			case 1:
-				puts("1");
-				print();
-				break;
-			case 2:
-				puts("2");
-				break;
-			default:
-				puts("error : recur");
-				exit(1);
-		}
-	}
+int main(){
+	Sudoku ss;
+	ss.readIn();
+	ss.solve();
+	return 0;
 }
